aa0d5f324d5bb7f1eb26ed7db1e718c70cb75c9f
Muhammad Zakarya <codecrafter302@gmail.com>
2025-12-08 01:19:08 +0500

    Remove comment from Eraser price output
    Correcting

--- Lab 2/Lab Two --Task no - 2.cpp
+++ Lab 2/Lab Two --Task no - 2.cpp
@@ -6,7 +6,7 @@ int main(){
 cout<<"Item \t Price \t quality \n"
     <<"Pen \t 1$\t 10/8 \n"
     <<"pencil \t 2$\t 10/7 \n"
-    <<"Eraser \t 0.5$\t 10/9\n";           //Task three ......lab 1
+    <<"Eraser \t 0.5$\t 10/9\n";           
 
 return 0;
 
